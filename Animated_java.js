850c84cfe2e5469810c297841e01a309146b4dc7
ss71550 <sophiahstewart@gmail.com>
2020-04-03 11:46:28 -0500

    Transfered code from code pen

---
+++ Animated_java.js
@@ -0,0 +1,31 @@
+$(document).ready(function (){
+  $("#start").click(function () {
+   $(".maze").animate({
+    height: "400px",
+    width: "600px"
+    });
+  });
+
+  $(document).keydown(function(e){
+    if (e.keycode=="40"){
+    $(".block").animate({
+       top: "+=20",
+  });
+    }
+    if (e.keycode=="38") {
+     $(".block").animate({
+       top: "-=20",
+  });
+    }
+  if (e.keycode=="39") {
+     $(".block").animate({
+       left: "+=20",
+  });
+  }
+  if (e.keycode=="37") {
+     $(".block").animate({
+       left: "-=20",
+  });
+  }
+  });
+})
