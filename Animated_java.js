8fc1703e06d19a5351173fe1f49f8c979d36d120
ss71550 <sophiahstewart@gmail.com>
2020-04-03 12:05:49 -0500

    fixed commas

--- Animated_java.js
+++ Animated_java.js
@@ -9,22 +9,22 @@ $(document).ready(function (){
   $(document).keydown(function(e){
     if (e.keycode=="40"){
     $(".block").animate({
-       top: "+=20",
+       top: "+=20"
   });
     }
     if (e.keycode=="38") {
      $(".block").animate({
-       top: "-=20",
+       top: "-=20"
   });
     }
   if (e.keycode=="39") {
      $(".block").animate({
-       left: "+=20",
+       left: "+=20"
   });
   }
   if (e.keycode=="37") {
      $(".block").animate({
-       left: "-=20",
+       left: "-=20"
   });
   }
   });
